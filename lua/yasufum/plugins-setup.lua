cb1c34bfb6df4a209c66d7531e350fa426b0dff1
Yasufumi Ogawa <yasufum.o@gmail.com>
2023-03-17 15:40:58 +0900

    Add completion and snippet plugins

--- lua/yasufum/plugins-setup.lua
+++ lua/yasufum/plugins-setup.lua
@@ -49,6 +49,18 @@ local plugins = {
     dependencies = {"nvim-lua/plenary.nvim"},
   },
 
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    }
+  },
+
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  "rafamadriz/friendly-snippets",
+
   "nvim-lualine/lualine.nvim",
   "numToStr/Comment.nvim",
   "tpope/vim-surround",
